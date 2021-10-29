ff311e4e15e9cd4de2308b0bc5347301808f55f1
german_job <german1199@inbox.ru>
2021-10-29 15:05:11 +0300

    chat

--- js/script.js
+++ js/script.js
@@ -3,7 +3,13 @@
 window.onload = function () {
 
     //Инициализация плеера
-    videojs('my-player');
+    videojs('my-player', {
+        controlBar: {
+            'liveDisplay': true,
+            'pictureInPictureToggle': false,
+            'fullscreenToggle': false,
+        }
+    });
     let player = videojs('my-player');
 
     //chat по кнопке play
